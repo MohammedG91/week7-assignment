b246d856646af4d096118b7bd53dcdbe9e7ce3fe
MohammedG91 <mo.gelu@gmail.com>
2025-02-02 20:34:15 +0000

    added image to page

---
+++ client/src/components/Image.jsx
@@ -0,0 +1,10 @@
+export default function image() {
+  return (
+    <div className="image-container">
+      <img
+        src="https://hips.hearstapps.com/hmg-prod/images/wedding-wishes-bride-and-groom-surrounded-by-their-friends-66abb2eac5cde.jpg"
+        alt="A joyous couple on the happiest day of their lives. A beautiful wedding"
+      />
+    </div>
+  );
+}
